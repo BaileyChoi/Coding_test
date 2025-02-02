2ba2243f490f90dc5d597743f50d68572433060d
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-02-02 15:15:03 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,13 +1,4 @@
 function solution(n) {
-    let limit = 50000000000000;
-    const square = new Set();
-    
-    let i = 1;
-    while (i * i <= limit) {
-        square.add(i * i);
-        i++;
-    }
-    
-    if (square.has(n)) return Math.pow(Math.sqrt(n) + 1, 2);
+    if (Number.isInteger(Math.sqrt(n))) return Math.pow(Math.sqrt(n) + 1, 2);
     else return -1;
 }
