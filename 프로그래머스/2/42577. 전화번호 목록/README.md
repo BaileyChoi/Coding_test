3ff7e2a271049653129208916ab1df387df07ec9
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-04-20 14:48:02 +0900

    [level 2] Title: 전화번호 목록, Time: 339.30 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 98.1 MB, 시간: 355.92 ms
+메모리: 96.3 MB, 시간: 339.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 18일 17:48:10
+2025년 04월 20일 14:47:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,11 +4,20 @@ class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
         
+        // 방법 1
+        // for (int i = 0; i < phone_book.length - 1; i++) {
+        //     if (phone_book[i + 1].startsWith(phone_book[i])) {
+        //         return false;
+        //     }
+        // }
+        
+        // 방법 2
         for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
+            if (phone_book[i].length() < phone_book[i + 1].length() &&
+               phone_book[i].equals(phone_book[i + 1].substring(0, phone_book[i].length())))
                 return false;
-            }
         }
+        
         return true;
+        }
     }
-}
