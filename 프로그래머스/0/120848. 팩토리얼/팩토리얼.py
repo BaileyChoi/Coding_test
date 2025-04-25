23863ed877785a335626676b270b67c2f00886d5
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-04-25 10:13:12 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,15 +1,24 @@
-def factorial(k):
-    if k == 1:
-        return 1
-    return k * factorial(k - 1)
+# def factorial(k):
+#     if k == 1:
+#         return 1
+#     return k * factorial(k - 1)
     
-def solution(n):
-    answer = 1
+# def solution(n):
+#     answer = 1
     
-    while factorial(answer) <= n :
-        answer += 1   
+#     while factorial(answer) <= n :
+#         answer += 1   
 
-    return answer - 1
+#     return answer - 1
+
+def solution(n):
+    answer, fact = 1, 1
+    
+    while fact * (answer + 1) <= n:
+        answer += 1
+        fact *= answer
+    
+    return answer
         
     
     
