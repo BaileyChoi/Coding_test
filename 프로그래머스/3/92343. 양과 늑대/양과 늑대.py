d07d6d565d7212d57248958761b7598963ffb8d4
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-21 15:43:52 +0900

    [level 3] Title: 양과 늑대, Time: 7.01 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,31 +1,34 @@
 def solution(info, edges):
-    # 방문을 체크할 수 있는 배열을 선언
-    visited = [False] * len(info)
-    # 0번 노드를 방문한 상태로 시작
-    visited[0] = True
-    # DFS를 통해 방문한 경우의 수 중 가장 양을 많이 방문한 경우 반환
-    return dfs(info, edges, visited, 1, 0)
+    answer = 0
+    n = len(info)
+    graph = [[] for _ in range(n)]
 
-def dfs(info, edges, visited, sheep, wolf):
-    # 만약 늑대가 양보다 크거나 같은 빠져나옴
-    if sheep == wolf:
-        return sheep
-    max_sheep = sheep
+    for a, b in edges:
+        graph[a].append(b)
+        
+    next_nodes = set()
+    next_nodes.add(0)
     
-    # 모든 edge를 확인
-    for parent, child in edges:
-        # 부모가 방문한 상태고 자식이 아직 방문되지 않은 상태라면 방문
-        if visited[parent] and not visited[child]:
-            # 다음 노드 방문 표시
-            visited[child] = True
-            # 다음 노드가 양일 경우
-            if info[child] == 0:
-                max_sheep = max(max_sheep, dfs(info, edges, visited, sheep + 1, wolf))
-            # 늑대일 경우
-            else:
-                max_sheep = max(max_sheep, dfs(info, edges, visited, sheep, wolf + 1))
-            # 다음 노드 방문 표시 해제
-            visited[child] = False
+    def backtrack(cur, sheep, wolf, next_nodes):
+        nonlocal answer
+        
+        if info[cur]:
+            wolf += 1
+        else:
+            sheep += 1
+            
+        if wolf >= sheep:
+            return
+        
+        answer = max(answer, sheep)
+        
+        new_nodes = next_nodes.copy()
+        new_nodes.remove(cur)
+        new_nodes.update(graph[cur])
+        
+        for next_node in new_nodes:
+            backtrack(next_node, sheep, wolf, new_nodes)
     
-    return max_sheep
-                
+    backtrack(0, 0, 0, next_nodes)
+    
+    return answer
