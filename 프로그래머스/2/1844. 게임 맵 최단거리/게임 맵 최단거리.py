b66e5e309cf99ff425d04a248acfe08d1b1e7605
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-23 09:52:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.28 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,36 @@
 from collections import deque
 
-
 def solution(maps):
-    dx = [-1, 1, 0, 0]  # 행 이동(상하좌우)
-    dy = [0, 0, -1, 1]  # 열 이동(상하좌우)
-
-    n = len(maps)  # 행 길이
-    m = len(maps[0])  # 열 길이
-
+    # BFS 최단거리 구하기
+    n = len(maps)
+    m = len(maps[0])
     queue = deque()
     visited = [[False] * m for _ in range(n)]
-
-    queue.append((0, 0))  # 시작점
+    
+    
+    # 시작점 예약 (0, 0, 거리 1)
+    queue.append([0, 0, 1])
     visited[0][0] = True
+    
+    dr = [0, 0, -1, 1]
+    dc = [-1, 1, 0, 0]
 
     while queue:
-        x, y = queue.popleft()
-
+        # 현재 노드 방문
+        [cur_row, cur_col, dist] = queue.popleft()
+        
+        # if 도착지 도달하면 return dist
+        if cur_row == n - 1 and cur_col == m - 1:
+            return dist
+    
+        # 다음 노드 탐색    
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            # 갈 수 있는 길인지 확인
-            if (
-                0 <= nx < n
-                and 0 <= ny < m
-                and maps[nx][ny] == 1
-                and not visited[nx][ny]
-            ):
-                visited[nx][ny] = True
-                queue.append((nx, ny))
-                maps[nx][ny] = maps[x][y] + 1
-
-    return maps[n - 1][m - 1] if maps[n - 1][m - 1] != 1 else -1
+            new_row = cur_row + dr[i]
+            new_col = cur_col + dc[i]
+            
+            if 0 <= new_row < n and 0 <= new_col < m and maps[new_row][new_col] == 1:
+                if not visited[new_row][new_col]:
+                    queue.append([new_row, new_col, dist + 1])
+                    visited[new_row][new_col] = True
+                
+    return -1
