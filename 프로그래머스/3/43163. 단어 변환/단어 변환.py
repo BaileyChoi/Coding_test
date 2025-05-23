01ed50ca0b27844ccfb304eb691fd24618edab25
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-23 10:55:38 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,31 +1,35 @@
 from collections import deque
 
+def getDiffCount(wordA, wordB):
+    diffCount = 0
+    for i in range(len(wordA)):
+        if wordA[i] != wordB[i]:
+            diffCount += 1
+    
+    return diffCount
 
 def solution(begin, target, words):
+    # BFS로 최단 거리 구하기
     queue = deque()
     visited = set()
-
-    queue.append((begin, 0))
-
+    
+    # 시작점 예약 ( 문자열 begin )
+    queue.append([begin, 0])
+    visited.add(begin)
+    
     while queue:
-        curWord, steps = queue.popleft()
-
-        # target 단어 찾았을 경우
-        if curWord == target:
-            return steps
-
-        # 다음 단어 탐색
-        for nextWord in words:
-            if nextWord not in visited and getDiffCount(curWord, nextWord) == 1:
-                queue.append((nextWord, steps + 1))
-                visited.add(nextWord)
-
+        # 현재 노드 방문
+        [cur_word, count] = queue.popleft()
+        
+        # if 현재 노드 == target: return count;
+        if cur_word == target:
+            return count
+    
+        # 다음 노드 방문
+        for next_word in words:
+            if getDiffCount(cur_word, next_word) == 1:
+                if next_word not in visited:
+                    queue.append([next_word, count + 1])
+                    visited.add(next_word)
+        
     return 0
-
-
-def getDiffCount(w1, w2):
-    diffCount = 0
-    for i in range(len(w1)):
-        if w1[i] != w2[i]:
-            diffCount += 1
-    return diffCount
