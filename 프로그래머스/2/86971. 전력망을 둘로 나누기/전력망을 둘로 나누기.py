1634b1e98e847b091a21170e18bb24786be34e93
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-25 16:27:16 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.14 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,27 +1,51 @@
+from collections import deque
 def solution(n, wires):
-    min_diff = n
+    answer = n
     
-    def dfs(start, visited, graph):
-        visited[start] = True
-        count = 1
+    # 그래프 만들기
+    graph = [[] for _ in range(n + 1)]
+
+    for a, b in wires:
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    # 서브트리 개수 구하기
+    def bfs(start, graph):
+        count = 0
+        queue = deque()
+        visited = set()
         
-        for neighbor in graph[start]:
-            if not visited[neighbor]:
-                count += dfs(neighbor, visited, graph)
-    
+        queue.append(start)
+        visited.add(start)
+        
+        while queue:
+            cur = queue.popleft()
+            count += 1
+            
+            for next in graph[cur]:
+                if next not in visited:
+                    queue.append(next)
+                    visited.add(next)
+        
         return count
-        
-        
-    for i in range(len(wires)):
-        graph = [[] for _ in range(n + 1)]
-        for j, (a, b) in enumerate(wires):
-            if i == j:
-                 continue
-            graph[a].append(b)
-            graph[b].append(a)
-        
-        visited = [False] * (n + 1)
-        subtree_size = dfs(1, visited, graph)
-        min_diff = min(min_diff, abs(n - subtree_size * 2))
     
-    return min_diff
+    # for a, b in wires: 
+    for a, b in wires:
+        # 전선 끊기
+        graph[a].remove(b)
+        graph[b].remove(a)
+        
+        # dfs로 전력망 개수 구하기
+        count1 = bfs(a, graph)
+        
+        # 전선 다시 붙이기
+        graph[a].append(b)
+        graph[b].append(a)
+
+        # 최소 차이 갱신
+        count2 = n - count1 
+        cur_diff = abs(count1 - count2)
+        answer = min(answer, cur_diff)
+        
+    return answer
+    
