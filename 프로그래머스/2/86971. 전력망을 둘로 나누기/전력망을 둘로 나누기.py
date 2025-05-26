9d2c2f98835401539b5da0320c7d72220abc4fad
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-26 15:50:12 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.33 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,51 +1,43 @@
-from collections import deque
 def solution(n, wires):
-    answer = n
-    
-    # 그래프 만들기
+    min_diff = float('inf')
+
+    # def dfs: 한 네트워크의 송전탑 개수 구하기
+    def dfs(start, visited, graph):
+        visited.add(start)
+        count = 1
+        for next in graph[start]:
+            if next not in visited:
+                count += dfs(next, visited, graph)
+        return count
+
+    # 전력망 네트워크 생성
     graph = [[] for _ in range(n + 1)]
 
     for a, b in wires:
         graph[a].append(b)
         graph[b].append(a)
-    
-    # 서브트리 개수 구하기
-    def bfs(start, graph):
-        count = 0
-        queue = deque()
-        visited = set()
-        
-        queue.append(start)
-        visited.add(start)
-        
-        while queue:
-            cur = queue.popleft()
-            count += 1
-            
-            for next in graph[cur]:
-                if next not in visited:
-                    queue.append(next)
-                    visited.add(next)
-        
-        return count
-    
-    # for a, b in wires: 
+
+    # 전선 끊기
     for a, b in wires:
-        # 전선 끊기
         graph[a].remove(b)
         graph[b].remove(a)
+
+        # dfs로 송전탑 개수 구하기
+        visited = set()
+        count1 = dfs(a, visited, graph)
+        count2 = n - count1
         
-        # dfs로 전력망 개수 구하기
-        count1 = bfs(a, graph)
-        
-        # 전선 다시 붙이기
+        # 전선 다시 연결
         graph[a].append(b)
         graph[b].append(a)
 
-        # 최소 차이 갱신
-        count2 = n - count1 
-        cur_diff = abs(count1 - count2)
-        answer = min(answer, cur_diff)
-        
-    return answer
+        # 최소 송전탑 개수의 차이 갱신
+        diff = abs(count1 - count2)        
+        min_diff = min(min_diff, diff)
     
+    return min_diff
+
+# 테스트
+print(solution(9, [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))
+print(solution(4, [[1,2],[2,3],[3,4]]))
+print(solution(7, [[1,2],[2,7],[3,7],[3,4],[4,5],[6,7]]))
