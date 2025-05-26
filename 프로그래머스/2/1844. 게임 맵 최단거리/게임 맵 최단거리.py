b0b582020da9bdb86650741098a6ac06eaaf35f5
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-26 14:35:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.31 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,36 +1,39 @@
 from collections import deque
 
 def solution(maps):
-    # BFS 최단거리 구하기
-    n = len(maps)
-    m = len(maps[0])
+    # BFS로 최단거리 구하기
+    N = len(maps)
+    M = len(maps[0])
+
     queue = deque()
-    visited = [[False] * m for _ in range(n)]
-    
-    
-    # 시작점 예약 (0, 0, 거리 1)
-    queue.append([0, 0, 1])
+    visited = [[False] * M for _ in range(N)]
+
+    # 시작점 예약(curR, curC, dist)
+    queue.append((0, 0, 1))
     visited[0][0] = True
-    
+
     dr = [0, 0, -1, 1]
     dc = [-1, 1, 0, 0]
 
     while queue:
         # 현재 노드 방문
-        [cur_row, cur_col, dist] = queue.popleft()
-        
-        # if 도착지 도달하면 return dist
-        if cur_row == n - 1 and cur_col == m - 1:
+        curR, curC, dist = queue.popleft()
+
+        # if 도착지 방문: return dist
+        if curR == N - 1 and curC == M - 1:
             return dist
-    
-        # 다음 노드 탐색    
+
+        # 다음 노드 예약
         for i in range(4):
-            new_row = cur_row + dr[i]
-            new_col = cur_col + dc[i]
-            
-            if 0 <= new_row < n and 0 <= new_col < m and maps[new_row][new_col] == 1:
-                if not visited[new_row][new_col]:
-                    queue.append([new_row, new_col, dist + 1])
-                    visited[new_row][new_col] = True
-                
+            nextR, nextC = curR + dr[i], curC + dc[i]
+            if 0 <= nextR < N and 0 <= nextC < M and maps[nextR][nextC] == 1:
+                if not visited[nextR][nextC]:
+                    visited[nextR][nextC] = True
+                    queue.append((nextR, nextC, dist + 1))
     return -1
+
+# 테스트
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
+print(solution(maps))
+maps = [[[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]]
+print(solution(maps))
