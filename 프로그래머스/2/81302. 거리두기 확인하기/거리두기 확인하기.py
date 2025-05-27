5bae54edd27345cb82366780b4719ab176361c10
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-27 19:33:55 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.19 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -2,56 +2,56 @@ from collections import deque
 
 def solution(places):
     answer = []
-    
-    # 대기실에서 'P'를 만나면 bfs 실행하여 거리두기 위반 여부 체크 
+
+    # def bfs: 거리두기가 지켜지고 있는지 확인
+    def bfs(start, board):   
+        dr = [0, 0, -1, 1]
+        dc = [-1, 1, 0, 0]
+
+        queue = deque()
+        visited = [[False] * 5 for _ in range(5)]
+
+        # 시작점 예약
+        queue.append((start, 0))
+        visited[start[0]][start[1]] = True
+
+        while queue:
+            # 현재 노드 방문
+            (curR, curC), dist = queue.popleft()
+
+            # if 시작점이 아닌데 'P'를 만났고 거리가 2 이하라면 위반
+            if dist != 0 and board[curR][curC] == 'P' and dist <= 2:
+                return False
+            
+            # if 거리 2 초과면 cotinue
+            if dist >= 2:
+                continue
+
+            # 다음 노드 방문
+            for i in range(4):
+                nextR, nextC = curR + dr[i], curC + dc[i]
+
+                if 0 <= nextR < 5 and 0 <= nextC < 5 and board[nextR][nextC] != 'X':
+                    if not visited[nextR][nextC]:
+                        visited[nextR][nextC] = True
+                        queue.append(((nextR, nextC), dist + 1))
+
+        return True
+
     for room in places:
-        isValid = 1
-        for r in range(5):
-            for c in range(5):
-                if room[r][c] == 'P':
-                    if bfs(room, [r, c]) == 0:
-                        isValid = 0
+        is_valid = 1
+        for row in range(5):
+            for col in range(5):
+                if room[row][col] == 'P':
+                    if not bfs((row, col), room):
+                        is_valid = 0
                         break
-            if isValid == 0:
+            if is_valid == 0:
                 break
-        
-        answer.append(isValid)
-        
+
+        answer.append(is_valid)
+
     return answer
 
-def bfs(room, start):
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
-    
-    visited = [[False] * 5 for _ in range(5)]
-    dist = [[0] * 5 for _ in range(5)]
-    
-    queue = deque()
-    
-    queue.append(start)
-    visited[start[0]][start[1]] = True
-    
-    while queue:
-        # 현재 노드 방문
-        [curRow, curCol] = queue.popleft()
-        
-        # 시작 지점이 아닌데 'P'를 만났고, 거리가 2 이하라면 위반
-        if dist[curRow][curCol] != 0 and room[curRow][curCol] == 'P' and dist[curRow][curCol] <= 2:
-            return 0
-        
-        # 거리 2 초과면 더 탐색할 필요X
-        if dist[curRow][curCol] >= 2:
-            continue
-        
-        # 다음 노드 방문
-        for i in range(4):
-            nextRow = curRow + dr[i]
-            nextCol = curCol + dc[i]
-            
-            if 0 <= nextRow < 5 and 0 <= nextCol < 5 and room[nextRow][nextCol] != 'X':
-                if not visited[nextRow][nextCol]:
-                    queue.append([nextRow, nextCol])
-                    visited[nextRow][nextCol] = True
-                    dist[nextRow][nextCol] = dist[curRow][curCol] + 1
-    
-    return 1
+# 테스트
+print(solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]))
