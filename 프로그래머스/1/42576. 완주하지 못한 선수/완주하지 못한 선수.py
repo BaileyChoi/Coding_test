5c38530262d5c824bd6e00e9590a51941720a73b
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-29 16:26:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.81 ms, Memory: 25.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,9 @@
-def solution(participants, completion):
-    participants.sort()
+def solution(participant, completion):
+    participant.sort()
     completion.sort()
-
-    for p, c in zip(participants, completion):
+    
+    for p, c in zip(participant, completion):
         if p != c:
             return p
         
-    return participants[-1]
-
-# 테스트
-print(solution(["leo", "kiki", "eden"],	["eden", "kiki"]))
-print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
-print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
+    return participant[-1]
