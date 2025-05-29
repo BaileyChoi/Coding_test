f1e3e7dae13b50db90a14eb34848d1d27563ca22
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-29 22:51:11 +0900

    [level 3] Title: 단어 변환, Time: 0.25 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,41 +1,32 @@
 from collections import deque
 
 def solution(begin, target, words):
-    # BFS로 최단 변환수 구하기
-    N = len(words)
-
     queue = deque()
     visited = set()
-
+    
     def canChange(word1, word2):
-        count = 0
+        diff = 0
         for i in range(len(word1)):
             if word1[i] != word2[i]:
-                count += 1
-            if count > 1:
+                diff += 1
+            if diff >= 2:
                 return False
         return True
-            
-    # 시작점 예약 (cur, count)
+    
+    
     queue.append((begin, 0))
     visited.add(begin)
-
+    
     while queue:
-        # 현재 노드 방문
         cur, count = queue.popleft()
-
-        # if cur == target: return count
+        
         if cur == target:
             return count
         
-        # 다음 노드 예약
         for word in words:
-            if word not in visited and canChange(cur, word):
-                visited.add(word)
-                queue.append((word, count + 1))
-
+            if canChange(cur, word):
+                if word not in visited:
+                    visited.add(word)
+                    queue.append((word, count + 1))
+                    
     return 0
-
-# 테스트
-print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"])) 
-print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"])) 
