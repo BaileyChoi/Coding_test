065cdce8834347f513862a1b3df4ad8e3377eca6
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-05-29 22:46:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.88 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,34 +1,30 @@
 from collections import deque
 
 def solution(maps):
-    # BFS로 최단 거리 구하기
-    rowLen = len(maps)
-    colLen = len(maps[0])
+    lenR = len(maps)
+    lenC = len(maps[0])
+    
+    queue = deque()
+    visited = [[False] * lenC for _ in range(lenR)]
+    
+    queue.append((0, 0, 1))
+    visited[0][0] = True
     
     dr = [0, 0, -1, 1]
     dc = [-1, 1, 0, 0]
     
-    queue = deque()
-    visited = [[False] * colLen for _ in range(rowLen)]
-    
-    # 시작점 예약 (curR, curC, dist)
-    queue.append((0, 0, 1))
-    visited.append((0, 0))
-    
     while queue:
-        # 현재 노드 방문 
         curR, curC, dist = queue.popleft()
         
-        # if 도착지 방문:
-        if curR == rowLen - 1 and curC == colLen - 1:
+        if curR == lenR - 1 and curC == lenC - 1:
             return dist
         
-        # 다음 노드 방문
         for i in range(4):
             nextR, nextC = curR + dr[i], curC + dc[i]
-            if 0 <= nextR < rowLen and 0 <= nextC < colLen and maps[nextR][nextC] == 1:
+            
+            if 0<=nextR<lenR and 0<=nextC<lenC and maps[nextR][nextC] == 1:
                 if not visited[nextR][nextC]:
                     visited[nextR][nextC] = True
                     queue.append((nextR, nextC, dist + 1))
-                    
+    
     return -1
