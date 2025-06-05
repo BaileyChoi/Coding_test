82cc897ee1ed79cf29d27a41de4ecbc2e77156c8
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-06-05 14:53:22 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,20 +1,20 @@
 def solution(name):
-    n = len(name)
-
-    # 문자 변경
-    moves = 0
+    # 위/아래 알파벳 조작 최솟값 
+    minAlpha = 0
     for char in name:
-        moves += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
-    
-    # 커서 이동
-    min_move = n - 1
+        minAlpha += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+    # 커서 이동 최소 경로
+    n = len(name)
+    minCursor = n - 1
     for i in range(n):
+        # i 이후 연속된 A의 개수 확인
         next_idx = i + 1
-        # 연속된 A 구간 건너뛰기
         while next_idx < n and name[next_idx] == 'A':
             next_idx += 1
-        # 좌우 이동 최솟값 갱신
-        move = i + n - next_idx + min(i, n - next_idx)
-        min_move = min(min_move, move)
+        # 오른쪽으로 i만큼, 다시 왼쪽으로 돌아와서 남은 부분 이동
+        move1 = i + i + (n - next_idx)
+        # 왼쪽으로 돌아서 갔다가 오른쪽으로 돌아서 i까지
+        move2 = (n - next_idx) * 2 + i
+        minCursor = min(minCursor, move1, move2)
     
-    return moves + min_move
+    return minAlpha + minCursor
