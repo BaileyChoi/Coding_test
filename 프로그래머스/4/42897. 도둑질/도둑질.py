57c7cfabd8d998ee6e10480832454a12020c30c5
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-07-04 17:22:14 +0900

    [level 4] Title: 도둑질, Time: 637.82 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,7 +1,18 @@
-def rob(m):
-    s = c = 0 
-    for n in m:
-        s,c = c, max(c, s+n)
-    return c
 def solution(money):
-    return max(rob(money[1:]), rob(money[:-1]))
+    
+    def rob(arr):
+        n = len(arr)
+        
+        dp = [0] * n
+        dp[0] = arr[0]
+        dp[1] = max(arr[0], arr[1])
+        
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + arr[i])
+            
+        return dp[-1]
+
+    if len(money) == 1:
+        return money[0]
+    
+    return max(rob(money[:-1]), rob(money[1:]))
