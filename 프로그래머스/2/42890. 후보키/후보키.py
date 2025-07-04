695566b07b3ec97274bb96f4d9a2e5b7be155079
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-07-04 11:08:14 +0900

    [level 2] Title: 후보키, Time: 4.00 ms, Memory: 9.58 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.py
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -1,33 +1,24 @@
 from itertools import combinations
 
 def solution(relation):
-    candidates = []
+    row = len(relation)
+    col = len(relation[0])
+
+    candy_keys = []
     
-    row_len = len(relation)
-    col_len = len(relation[0])
-    
-    # for 모든 컬럼 조합 검사
-    nums = [i for i in range(col_len)]
-    for i in range(col_len):
-        # for 조합
-        for comb in combinations(nums, i + 1):
-            # for 유일성 체크
-            projected_rows = set()
-            for row in relation:
-                projected_row = tuple(row[j] for j in comb)
-                projected_rows.add(projected_row)
-            # if 만족성 
-            if len(projected_rows) == row_len:
-                is_minimal = True
+    for i in range(1, col + 1):
+        for comb in combinations(range(col), i):
+            temp = [tuple(item[c] for c in comb) for item in relation]
             
-                # for 최소성 체크
-                for cand in candidates:
-                    if set(cand).issubset(set(comb)):
+            if len(set(temp)) == row:
+                is_minimal = True
+                   
+                for key in candy_keys:
+                    if set(key).issubset(set(comb)):
                         is_minimal = False
                         break
-                        
-                # if 만족 append
+                   
                 if is_minimal:
-                    candidates.append(comb)
-                    
-    return len(candidates)
+                   candy_keys.append(comb)
+    
+    return len(candy_keys)
