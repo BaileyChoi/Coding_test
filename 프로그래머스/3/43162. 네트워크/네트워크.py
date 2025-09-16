ec360efea2bc740b2377d5a9a6acaabeb23b22a0
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-09-16 16:38:01 +0900

    [level 3] Title: 네트워크, Time: 0.70 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,20 +1,18 @@
 def solution(n, computers):
-    answer = 0
-
-    def dfs(cur, visited, computers):
-        visited.add(cur)
+    total = 0
+    visited = [False] * n
+    
+    def dfs(start, visited, graph):
+        visited[start] = True
+        
         for next in range(n):
-            if computers[cur][next] == 1 and next not in visited:
-                dfs(next, visited, computers)
-
-    visited = set()
+            if not visited[next] and graph[start][next] == 1:
+                dfs(next, visited, graph)
+    
     for i in range(n):
-        if i not in visited:
-            dfs(i, visited, computers) 
-            answer += 1
-
-    return answer
-
-# 테스트
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
-print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
+        if visited[i]:
+            continue
+        dfs(i, visited, computers)
+        total += 1
+    
+    return total
