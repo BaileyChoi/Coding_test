e404a06ead3b402457240b3823b5a9a9f38c85df
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-10-07 18:07:40 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.27 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,18 +1,13 @@
-def divisors_num(num):
-    n = 0
-    for i in range(1, num + 1):
-        if num % i == 0:
-            n += 1
-            
-    return n
+import math
 
 def solution(left, right):
     answer = 0
     
     for i in range(left, right + 1):
-        if divisors_num(i) % 2 == 0:
-            answer += i
-        else:
+        r = int(math.sqrt(i))
+        if r * r == i:
             answer -= i
+        else:
+            answer += i
     
     return answer
