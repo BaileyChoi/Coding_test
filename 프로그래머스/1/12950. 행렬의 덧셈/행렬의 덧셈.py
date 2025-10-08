12244412bb0e8347181663a6a70a146f43f0056c
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-10-08 18:28:46 +0900

    [level 1] Title: 행렬의 덧셈, Time: 12.32 ms, Memory: 22 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,10 +1,2 @@
 def solution(arr1, arr2):
-    row_len = len(arr1)
-    col_len = len(arr1[0])
-    
-    answer = [[0] * col_len for _ in range(row_len)]
-    
-    for i in range(row_len):
-        for j in range(col_len):
-            answer[i][j] = arr1[i][j] + arr2[i][j]
-    return answer
+    return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(arr1, arr2)]
