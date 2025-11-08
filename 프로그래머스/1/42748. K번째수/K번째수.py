fd8ed4ebd72e6bd0cd6899ed6298eaeca83cc708
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-08 22:46:15 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,8 +2,6 @@ def solution(array, commands):
     answer = []
     
     for start, end, order in commands:
-        cut_array = array[start-1:end]
-        sort_array = sorted(cut_array)
-        num = sort_array[order-1]
-        answer.append(num)
+        answer.append(sorted(array[start - 1:end])[order - 1])
+        
     return answer
