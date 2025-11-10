077a26fe36fe943c6e24264c7002b4b565ccda5e
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-10 22:24:30 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,2 @@
 def solution(nums):
-    n = len(nums) // 2
-    pocket_set = set(nums)
-    
-    return len(pocket_set) if len(pocket_set) <= n else n
+    return min(len(set(nums)), len(nums) // 2)
