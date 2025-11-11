ad882204d5460aa2930d83b6f3a38354bec7250a
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-11 21:11:35 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,10 +2,10 @@ def solution(citations):
     n = len(citations)
     citations.sort()
     
-    if citations[-1] == 0:
-            return 0
-    
-    for h in range(n, -1, -1):
-        if citations[n - h] >= h:
+    for i, c in enumerate(citations):
+        h = n - i
+        if c >= h:
             return h
+    
+    return 0
     
