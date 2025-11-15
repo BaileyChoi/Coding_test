60012b36d70db1f6628fd6d5ad17c5c7c433c839
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-15 19:01:42 +0900

    [level 1] Title: 모의고사, Time: 2.43 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,17 +1,24 @@
 def solution(answers):
-    patterns = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ]
+    answer = []
+    right = [0, 0, 0]
     
-    scores = [0, 0, 0]
+    p1 = [1,2,3,4,5]
+    p2 = [2,1,2,3,2,4,2,5]
+    p3 = [3,3,1,1,2,2,4,4,5,5]
     
-    for i, answer in enumerate(answers):
-        for j, p in enumerate(patterns):
-            if p[i % len(p)] == answer:
-                scores[j] += 1
+    for i, num in enumerate(answers):
+        if num == p1[i % 5]:
+            right[0] += 1
+        if num == p2[i % 8]:
+            right[1] += 1
+        if num == p3[i % 10]:
+            right[2] += 1
     
-    max_score = max(scores)
+    max_right = max(right)
+    for i, val in enumerate(right):
+        if val == max_right:
+            answer.append(i + 1)
+
+    return answer
         
-    return [i + 1 for i, score in enumerate(scores) if score == max_score]
+    
