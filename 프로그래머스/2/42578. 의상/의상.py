6f849859371c6cdbc42a4cb709f1248a79abb736
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-15 18:02:14 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,13 @@
+from collections import defaultdict
+
 def solution(clothes):
-    # 종류별 개수 세기
-    type_count = {}
-    for name, type in clothes:
-        type_count[type] = type_count.get(type, 0) + 1
-        
-    # 종류마다 경우의 수
     answer = 1
-    for count in type_count.values():
-        answer *= (count + 1)
-        
+    closet = defaultdict(list)
+    
+    for cloth, cloth_type in clothes:
+        closet[cloth_type].append(cloth)
+    
+    for value in closet.values():
+        answer *= (len(value) + 1)
+    
     return answer - 1
-        
-        
