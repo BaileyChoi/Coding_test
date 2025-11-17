4bb8af253d7c1f13acd4acd21fd5c1cfe9a604ff
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-17 19:09:35 +0900

    [level 2] Title: 피로도, Time: 35.41 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -2,15 +2,18 @@ from itertools import permutations
 
 def solution(k, dungeons):
     answer = 0
+    explores = permutations(dungeons, len(dungeons))
     
-    for p in permutations(dungeons, len(dungeons)):
-        exhaust, succeed = k, 0
-        for need, cost in p:
+    for explore in explores:
+        exhaust = k
+        success = 0
+        for need, consume in explore:
             if exhaust < need:
-                break
-            else:
-                exhaust -= cost
-                succeed += 1
-        answer = max(answer, succeed)
+                continue
+            exhaust -= consume
+            if exhaust >= 0:
+                success += 1
+                
+        answer = max(answer, success)
     
     return answer
