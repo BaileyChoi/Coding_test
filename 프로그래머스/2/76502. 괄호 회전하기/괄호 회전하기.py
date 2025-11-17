ba99fb4900e85517919d99a95e1d671a0e530234
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-17 17:53:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 215.64 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,25 +1,20 @@
-def isValid(s):
-    stack = []
-    pairs = {')': '(', ']': '[', '}': '{'}
-    
-    for char in s:
-        if char in list(pairs.values()):
-            stack.append(char)
+def is_correct(s):
+    new_s = ""
+    for c in s:
+        if new_s and ((new_s[-1] == "(" and c == ")") or (new_s[-1] == "[" and c == "]") or (new_s[-1] == "{" and c == "}")):
+            new_s = new_s[:-1]
         else:
-            if not stack or stack.pop() != pairs[char]:
-                return False
-            
-    return not stack
+            new_s += c
+    
+    return True if not new_s else False
 
 def solution(s):
     answer = 0
     
-    # 회전시키기
-    for i in range(len(s)):
-        newS = s[i:] + s[:i]
-        # 올바른 괄호 문자열인지 확인
-        if isValid(newS):
+    for _ in range(len(s)):        
+        if is_correct(s):
             answer += 1
+            
+        s = s[1:] + s[0]
     
-    return answer
-    
+    return answer 
