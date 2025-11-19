6d3d577e3476aceb7371c162e3210f09f042ff4f
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-19 17:08:26 +0900

    [level 1] Title: 모의고사, Time: 6.35 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -6,19 +6,14 @@ def solution(answers):
     p2 = [2,1,2,3,2,4,2,5]
     p3 = [3,3,1,1,2,2,4,4,5,5]
     
-    for i, num in enumerate(answers):
-        if num == p1[i % 5]:
-            right[0] += 1
-        if num == p2[i % 8]:
-            right[1] += 1
-        if num == p3[i % 10]:
-            right[2] += 1
+    for i, val in enumerate(answers):
+        right[0] += val == p1[i % 5]
+        right[1] += val == p2[i % 8]
+        right[2] += val == p3[i % 10]
     
     max_right = max(right)
     for i, val in enumerate(right):
         if val == max_right:
             answer.append(i + 1)
-
-    return answer
-        
     
+    return answer
