f8ee061baa850d48b13a5f684120e673cbfa9125
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-19 19:15:51 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 5.03 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,30 +1,24 @@
 def parse(file):
-    i = 0
     # head 찾기
-    while i < len(file):
-        if file[i].isdigit():
-            break
+    i = 0
+    while i < len(file) and not file[i].isdigit():
         i += 1
-    head = file[:i]
 
-    k = i
     # number 찾기
-    while k < len(file):
-        if not file[k].isdigit():
-            break
+    k = i
+    while k < len(file) and file[k].isdigit() and (k - i) < 5:
         k += 1
-    number_str = file[i:k]
     
-    return head, number_str
+    return file[:i], file[i:k]
 
 def solution(files):    
     # 파싱
     parsed = []
-    for i, file in enumerate(files):
+    for file in files:
         head, num = parse(file)
-        parsed.append((head.lower(), int(num), i, file))
+        parsed.append((head.lower(), int(num), file))
     
     # 정렬
-    parsed.sort(key=lambda name: (name[0], name[1], name[2]))
+    parsed.sort(key=lambda name: (name[0], name[1]))
     
-    return [p[3] for p in parsed]
+    return [p[2] for p in parsed]
