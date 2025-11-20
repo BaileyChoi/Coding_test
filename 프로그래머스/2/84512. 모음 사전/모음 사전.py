4f3e47ee7183e40e9611b1e3631501c71b468869
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-20 17:41:05 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,9 +1,10 @@
 def solution(word):
     answer = 0
-    order = {'A': 0, 'E': 1, 'I': 2, 'O': 3, 'U': 4}
+    
+    letters = ['A', 'E', 'I', 'O', 'U']
     weights = [781, 156, 31, 6, 1] 
     
-    for i, ch in enumerate(word):
-        answer += weights[i] * order[ch] + 1
-        
+    for i, c in enumerate(word):
+        answer += weights[i] * letters.index(c)+ 1
+    
     return answer
