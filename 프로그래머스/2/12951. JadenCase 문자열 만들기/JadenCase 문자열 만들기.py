b5e5d255d1c7a99762dd87ed130c92fcd55546f9
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-20 16:38:53 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,14 +1,2 @@
 def solution(s):
-    answer = s[0].upper()
-    
-    i, k = 1, 1
-    while k < len(s):
-        if s[k - 1] == ' ' and s[k] != ' ':
-            converted = s[i: k].lower() + s[k].upper() 
-            answer += converted
-            i = k + 1 
-        k += 1
-    
-    answer += s[i:].lower()
-    
-    return answer
+    return ' '.join([word.capitalize() for word in s.split(" ")])
