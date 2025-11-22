cb8b134acba9cd89eeaa380961942bd7ef69a958
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-22 18:15:26 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,8 +1,6 @@
 def solution(n):
     answer = n + 1
-    count_bin_n = bin(n).count('1')
-    
     while True:
-        if count_bin_n == bin(answer).count('1'):
+        if bin(n).count('1') == bin(answer).count('1'):
             return answer
         answer += 1
