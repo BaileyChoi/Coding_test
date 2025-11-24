a0889b1d81c05e5161ad4efc839d82918f265a2b
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-24 22:51:34 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,11 +1,8 @@
-import math
-
 def solution(n):
     answer = 0
     
     while n > 0:
-        if n % 2 == 1:
-            answer += 1
-        n = math.floor(n / 2)
+        answer += n % 2
+        n = n // 2
 
     return answer
