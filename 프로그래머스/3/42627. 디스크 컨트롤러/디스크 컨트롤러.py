7dbc211496526ca8333a19359dd7c147bcfbaf0b
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-30 18:50:43 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.66 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,29 @@
+import heapq
+
+def solution(jobs):
+    jobs.sort()
+    
+    n = len(jobs)
+    curTime = 0
+    total = 0
+    idx = 0
+    finished = 0
+    heap = []
+    
+    while finished < n:
+        while idx < n and jobs[idx][0] <= curTime:
+            s, l = jobs[idx]
+            heapq.heappush(heap, (l, s, idx))
+            idx += 1
+            
+        if not heap:
+            curTime = jobs[idx][0]
+            continue
+        
+        l, s, i = heapq.heappop(heap)
+        
+        curTime += l
+        total += curTime - s
+        finished += 1
+    
+    return total // n
