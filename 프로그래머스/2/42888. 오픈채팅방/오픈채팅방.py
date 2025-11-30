c7ff6c4778457bece8280727fd32d25761032ce4
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-11-30 15:33:58 +0900

    [level 2] Title: 오픈채팅방, Time: 109.06 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,23 +1,19 @@
-from collections import defaultdict
-
 def solution(record):
     answer = []
-    in_out = []
-    username = defaultdict(set)
+    username = {}
+    
+    for r in record:
+        sr = r.split(" ")
+        
+        if sr[0] in ["Enter", "Change"]:
+            username[sr[1]] = sr[2]
     
     for r in record:
         sr = r.split(" ")
         
         if sr[0] == "Enter":
-            in_out.append((sr[1], "님이 들어왔습니다."))
-            username[sr[1]] = sr[2]
+            answer.append(username[sr[1]] + "님이 들어왔습니다.")
         elif sr[0] == "Leave":
-            in_out.append((sr[1], "님이 나갔습니다."))
-        else:
-            username[sr[1]] = sr[2]
-    
-    for uid, did in in_out:
-        uname = username[uid]
-        answer.append(uname + did)
-    
+            answer.append(username[sr[1]] + "님이 나갔습니다.")
+            
     return answer
