3fb222164b2f5956827103157bbabeaa673eaa68
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-12-01 20:34:26 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 96.19 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,39 +1,28 @@
 from collections import deque
 
-
 def solution(queue1, queue2):
-    # queue1, queue2 큐로 만들기
-    q1 = deque(queue1)
-    q2 = deque(queue2)
-
-    # q1, q2의 초기 합 기록
-    sum1 = sum(q1)
-    sum2 = sum(q2)
-
-    # for 최대 수행 횟수
-    for i in range(4 * len(q1)):
-        # if 두 큐 합이 같을 경우:
+    answer = 0
+    n = len(queue1)
+    
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    
+    sum1 = sum(queue1)
+    sum2 = sum(queue2)
+    
+    while answer <= n * 4:
         if sum1 == sum2:
-            return i
-
-        # elif q1의 합이 더 클 경우:
+            return answer
         elif sum1 > sum2:
-            value = q1.popleft()
-            q2.append(value)
-            sum1 -= value
-            sum2 += value
-
-        # else q2의 합이 더 클 경우:
-        else:
-            value = q2.popleft()
-            q1.append(value)
-            sum2 -= value
-            sum1 += value
-
+            val = queue1.popleft()
+            queue2.append(val)
+            sum2 += val
+            sum1 -= val
+        elif sum1 < sum2:
+            val = queue2.popleft()
+            queue1.append(val)
+            sum1 += val
+            sum2 -= val
+        answer += 1
+            
     return -1
-
-
-# 테스트
-print(solution([3, 2, 7, 2], [4, 6, 5, 1]))
-print(solution([1, 2, 1, 2], [1, 10, 1, 2]))
-print(solution([1, 1], [1, 5]))
