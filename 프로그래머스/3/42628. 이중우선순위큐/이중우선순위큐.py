15f04f9e66bd26e5c5ea9232ec1d2a38853400e5
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-12-04 21:10:20 +0900

    [level 3] Title: 이중우선순위큐, Time: 749.85 ms, Memory: 15.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,24 @@
+import heapq
+
+def solution(operations):
+    heap = []
+    
+    for operation in operations:
+        do, num = operation.split(" ")
+        
+        if do == 'I':
+            heapq.heappush(heap, int(num))
+        elif do == 'D' and heap:
+            if num == "1":
+                heap = heapq.nlargest(len(heap), heap)[1:]
+                heapq.heapify(heap)
+            else:
+                heapq.heappop(heap)
+    
+    if heap:
+        return [max(heap), min(heap)]
+    else:
+        return [0, 0]
+    
+    
+    return answer
