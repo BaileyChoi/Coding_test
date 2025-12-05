a0b3f8c88859054c249c483987b35674b743fed0
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-12-05 18:30:27 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 1017.38 ms, Memory: 216 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,33 +1,24 @@
 def solution(sequence, k):
     answer = []
     
-    start, end = 0, 0
+    left, right = 0, 0
     n = len(sequence)
-    cur_sum = 0
-    min_len = n
-        
-    while start < n:
-        # if 합 < k:
-        if cur_sum < k:
-            if end == n:
-                break
-            cur_sum += sequence[end]
-            end += 1    
-        # elif 합 > k:
-        elif cur_sum > k:
-            cur_sum -= sequence[start]
-            start += 1
-        # else: 합 == k 조건 만족
-        else:
-            # 현재 구간 길이 체크
-            cur_len = end - 1 - start
-            # 최적 결과 갱신
-            if cur_len < min_len or (cur_len == min_len and start < answer[0]):
-                min_len = cur_len
-                answer = [start, end - 1]
-                    
-            # start 이동
-            cur_sum -= sequence[start]
-            start += 1
+    hap = sequence[0]
     
-    return answer
+    while left < n and right < n:
+        if hap < k:
+            right += 1
+            if right < n:
+                hap += sequence[right]
+        elif hap > k:
+            hap -= sequence[left]
+            left += 1
+        else:
+            answer.append([left, right])
+            right += 1
+            if right < n:
+                hap += sequence[right]
+    
+    return sorted(answer, key=lambda x: x[1] - x[0])[0]
+            
+    
