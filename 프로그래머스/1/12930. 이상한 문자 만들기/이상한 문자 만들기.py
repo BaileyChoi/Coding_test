2d4cafabee2e1cd93938a2ddeab8092b4c83fa48
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-12-09 16:25:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    
+    for word in s.split(" "):
+        for i, c in enumerate(word):
+            if i % 2 == 0:
+                answer += c.upper()
+            else:
+                answer += c.lower()
+        answer += ' '
+            
+    
+    return answer[:-1]
