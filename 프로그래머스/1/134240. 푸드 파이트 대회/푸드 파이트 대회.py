2c17a47b1c754fb211e0eeccfa6e82b4c3ce8cf8
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-12-11 18:40:28 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,11 @@
+def solution(food):
+    answer = ''
+    
+    for i in range(1, len(food)):
+        num = food[i] // 2
+        answer += str(i) * num 
+        
+    answer += '0'
+    answer += answer[-2::-1]
+    
+    return answer
