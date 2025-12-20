6060d67efc8345ad3daaccc506db794dd26b9b49
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-12-21 00:38:45 +0900

    [level 1] Title: 기사단원의 무기, Time: 1001.78 ms, Memory: 9.41 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,26 @@
+def attack(num):
+    count = 0
+    
+    if num == 1:
+        return 1
+    
+    for i in range(1, int(num ** 0.5) + 1):
+        if num % i == 0:
+            if i == num ** 0.5:
+                count += 1
+            else:
+                count += 2
+    
+    return count
+
+def solution(number, limit, power):
+    answer = 0
+    
+    for i in range(1, number + 1):
+        attackk = attack(i)
+        if attackk <= limit:
+            answer += attackk
+        else:
+            answer += power
+    
+    return answer
