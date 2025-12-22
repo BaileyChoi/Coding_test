258c83021cd960ae51288b9dc85d7bf0b427ad1e
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-12-22 21:17:24 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,17 +1,25 @@
 def solution(n, lost, reserve):
-    lost_set = set(lost)
-    reserve_set = set(reserve)
+    answer = 0
     
-    # 도난+여벌 겹치는 학생은 자기 것만 입음
-    overlap = lost_set & reserve_set
-    lost_set -= overlap
-    reserve_set -= overlap
+    own = [1] * n
     
-    # 왼쪽 -> 오른쪽 순 그리디로 대여
-    for r in sorted(reserve_set):
-        if r - 1 in lost_set:
-            lost_set.remove(r - 1)
-        elif r + 1 in lost_set:
-            lost_set.remove(r + 1)
-            
-    return n - len(lost_set)
+    for i in lost:
+        own[i - 1] -= 1
+        
+    for i in reserve:
+        own[i - 1] += 1
+        
+    for i, o in enumerate(own):
+        if o == 0:
+            if i - 1 >= 0 and own[i - 1] == 2:
+                own[i] += 1
+                own[i - 1] -= 1
+            elif i + 1 < n and own[i + 1] == 2:
+                own[i] += 1
+                own[i + 1] -= 1   
+  
+    for o in own:
+        if o > 0:
+            answer += 1
+    
+    return answer
