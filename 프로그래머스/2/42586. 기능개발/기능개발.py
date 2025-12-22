1cdff365b2b8ef1819d992d8f161c119a993c35a
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2025-12-22 21:29:02 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,15 +1,17 @@
-import math
+import math 
 
 def solution(progresses, speeds):
+    answer = {}
     days = []
+    
     for p, s in zip(progresses, speeds):
         days.append(math.ceil((100 - p) / s))
     
-    answer = {}
-    answer[days[0]] = 1
     for i in range(1, len(days)):
         if days[i] < days[i - 1]:
             days[i] = days[i - 1]
-        answer[days[i]] = answer.get(days[i], 0) + 1
-            
+        
+    for day in days:
+        answer[day] = answer.get(day, 0) + 1
+        
     return list(answer.values())
