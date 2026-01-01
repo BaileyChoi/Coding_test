aac23f1a09bf526f7c418ceb70bff738ce8eca45
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-01 23:26:52 +0900

    [level 1] Title: 과일 장수, Time: 139.47 ms, Memory: 20.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,10 @@
+def solution(k, m, score):
+    answer = 0
+    
+    score.sort(reverse=True)
+    
+    for i in range(len(score) // m):
+        box = score[i * m : (i + 1) * m]
+        answer += min(box) * len(box)
+        
+    return answer
