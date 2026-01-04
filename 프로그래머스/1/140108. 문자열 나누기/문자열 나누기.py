d325aa1976bfef85fbc754d13238c71d452ae267
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-04 15:41:24 +0900

    [level 1] Title: 문자열 나누기, Time: 0.67 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,22 +1,18 @@
 def solution(s):
     answer = 0
     
-    cur = 0
-    x = s[0]
-    count_x, diff = 1, 0
-    while (cur < len(s) - 1):
-        cur += 1
-        if (s[cur] == x):
-            count_x += 1
-        else:
-            diff += 1
+    x = ''
+    count_x, diff = 0, 0
+    
+    for c in s:
         if count_x == diff:
             answer += 1
-            if cur < len(s) - 1:
-                x = s[cur + 1]
-                count_x, diff = 0, 0
-    
-    if count_x != diff:
-        answer += 1
+            x = c
+            count_x, diff = 1, 0
+        else:
+            if c == x:
+                count_x += 1
+            else:
+                diff += 1
     
     return answer
