ddc89b5bb925e326f1ccd1d9345cacf506f6167e
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-11 15:48:43 +0900

    [level 1] Title: 신고 결과 받기, Time: 159.09 ms, Memory: 45.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,18 +1,15 @@
-from collections import defaultdict
-
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)
-    
     # 신고 처리) 신고당한 사람: [신고자, 신고자, ...]
-    report_set = defaultdict(set)
-    for r in report:
+    report_dict = {name: [] for name in id_list}
+    for r in set(report):
         reporter, reported = r.split(" ")
-        report_set[reported].add(reporter)
+        report_dict[reported].append(reporter)
         
     # 메일 건수 처리
-    for reported, reporter in report_set.items():
+    mail_dict = {name: 0 for name in id_list}
+    for reported, reporter in report_dict.items():
         if len(reporter) >= k:
             for r in reporter:
-                answer[id_list.index(r)] += 1
+                mail_dict[r] += 1
             
-    return answer
+    return list(mail_dict.values())
