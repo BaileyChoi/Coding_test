a74e5a725ae20acedc4b388d9698cf9c6910b19e
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-13 20:28:49 +0900

    [level 3] Title: 광고 삽입, Time: 697.69 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -0,0 +1,43 @@
+def to_sec(hms):
+    h, m, s = map(int, hms.split(":"))
+    return h * 3600 + m * 60 + s
+
+def to_hms(sec):
+    h = sec // 3600
+    sec %= 3600
+    m = sec // 60
+    s = sec % 60
+    return f"{h:02d}:{m:02d}:{s:02d}"
+    
+def solution(play_time, adv_time, logs):
+    play = to_sec(play_time)
+    adv = to_sec(adv_time)
+
+    diff = [0] * (play + 2)
+    for log in logs:
+        start_str, end_str = log.split("-")
+        start, end = to_sec(start_str), to_sec(end_str)
+        diff[start] += 1
+        diff[end] -= 1
+        
+    viewers = [0] * (play + 1)
+    cur = 0
+    for t in range(play):
+        cur += diff[t]
+        viewers[t] = cur
+        
+    prefix = [0] * (play + 2)
+    for t in range(play):
+        prefix[t + 1] = prefix[t] + viewers[t]
+        
+    best_start = 0
+    best_sum = prefix[adv] - prefix[0]
+    
+    for start in range(1, play - adv + 1):
+        end = start + adv
+        cur_sum = prefix[end] - prefix[start]
+        if cur_sum > best_sum:
+            best_sum = cur_sum
+            best_start = start
+            
+    return to_hms(best_start)
