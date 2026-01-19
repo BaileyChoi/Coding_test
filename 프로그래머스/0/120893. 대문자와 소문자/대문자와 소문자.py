127ce334bd164fb0627af33af4afb1c28a34717c
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-20 01:03:48 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,10 +1,2 @@
 def solution(my_string):
-    answer = ''
-    
-    for c in my_string:
-        if c.isupper():
-            answer += c.lower()
-        else:
-            answer += c.upper()
-    
-    return answer
+    return my_string.swapcase()
