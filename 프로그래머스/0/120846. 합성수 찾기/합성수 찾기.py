625bb1f3a54e67f6117b11584c68134301a72ed8
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-24 13:44:16 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,8 +1,10 @@
-def isHapsung(num):
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return 1
-    return 0
-
 def solution(n):
-    return sum(isHapsung(i) for i in range(4, n + 1))
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    
+    return sum(1 for x in range(4, n + 1) if not is_prime[x])
