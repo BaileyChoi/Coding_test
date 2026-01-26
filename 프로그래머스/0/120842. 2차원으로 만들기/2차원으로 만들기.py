936562e0988584740ad895da052668197ebfa706
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-26 14:10:19 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,8 +1,8 @@
 def solution(num_list, n):
-    answer = [[] for _ in range(len(num_list) // n)]
-    k = 0
-    for i in range(len(num_list) // n):
-        for j in range(n):
-            answer[i].append(num_list[k])
-            k += 1
+    answer = []
+    
+    for i in range(0, len(num_list), n):
+        row = num_list[i:i + n]
+        answer.append(row)
+        
     return answer
