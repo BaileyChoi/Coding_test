a1c2c3623e83ad314470ed262e87f7fedcb4e903
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-28 15:07:17 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,5 +1,6 @@
-from collections import Counter
-
 def solution(s):
-    cnt = Counter(s)
+    cnt = {}
+    for c in s:
+        cnt[c] = cnt.get(c, 0) + 1
+        
     return ''.join(c for c in sorted(cnt) if cnt[c] == 1)
