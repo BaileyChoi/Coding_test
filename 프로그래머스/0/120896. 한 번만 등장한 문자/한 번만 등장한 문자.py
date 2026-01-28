b93231d23b01597c5675a89d707e25830cbe199b
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-28 15:03:29 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,6 +1,2 @@
 def solution(s):
-    answer = ''
-    for c in sorted(set(s)):
-        if s.count(c) == 1:
-            answer += c
-    return answer
+    return ''.join(c for c in sorted(set(s)) if s.count(c) == 1)
