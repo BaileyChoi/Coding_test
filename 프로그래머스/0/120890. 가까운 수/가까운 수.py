2594fe97c0e660e726a73be11333357eb86e8b2c
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-01-31 22:04:57 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,11 +1,2 @@
 def solution(array, n):
-    answer = 0
-    diff = float('inf')
-    
-    for a in sorted(array):
-        cur = abs(a - n)
-        if cur < diff:
-            diff = cur
-            answer = a
-    
-    return answer
+    return sorted(array, key = lambda x:(abs(x - n), x))[0]
