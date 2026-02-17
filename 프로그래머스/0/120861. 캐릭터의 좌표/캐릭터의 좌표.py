d85b036a155bddd8590769cb572d35e52fd7e4e8
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-02-17 18:51:52 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,15 +1,12 @@
 def solution(keyinput, board):
-    answer = [0, 0]
-    direction = {"left": [-1, 0], "right": [1, 0], "up": [0, 1], "down": [0, -1]}
+    x, y = 0, 0
+    move = {"left": (-1, 0), "right": (1, 0), "up": (0, 1), "down": (0, -1)}
     
-    max_x = board[0] // 2
-    max_y = board[1] // 2
+    max_x, max_y = board[0] // 2, board[1] // 2
     
-    for key in keyinput:
-        nxt_x = answer[0] + direction[key][0]
-        nxt_y = answer[1] + direction[key][1]
-        
-        answer[0] = max(-max_x, min(max_x, nxt_x))
-        answer[1] = max(-max_y, min(max_y, nxt_y))
+    for k in keyinput:
+        nx, ny = x + move[k][0], y + move[k][1]
+        x = max(-max_x, min(max_x, nx))
+        y = max(-max_y, min(max_y, ny))
     
-    return answer
+    return [x, y]
