9a77fca27ac6fdebe92921585792757f669e287e
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-02-19 22:07:11 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,9 +1,2 @@
 def solution(chicken):
-    answer = 0
-    
-    while chicken >= 10:
-        service = chicken // 10
-        answer += service
-        chicken = service + (chicken % 10)
-        
-    return answer
+    return max(0, (chicken - 1) // 9)
