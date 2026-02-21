5a6761d026b3c98336e3dfdd2817e6f2c2cd6351
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-02-21 22:00:19 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,11 +1,14 @@
 -- 코드를 작성해주세요
 SELECT DISTINCT
-    D.ID, 
-    D.EMAIL, 
-    D.FIRST_NAME, 
-    D.LAST_NAME
-FROM DEVELOPERS D
-JOIN SKILLCODES S
-    ON D.SKILL_CODE & S.CODE > 0
-WHERE S.NAME IN ('Python', 'C#')
-ORDER BY D.ID;
+    ID, 
+    EMAIL, 
+    FIRST_NAME, 
+    LAST_NAME
+FROM DEVELOPERS 
+WHERE
+    SKILL_CODE & (
+        SELECT SUM(CODE)
+        FROM SKILLCODES
+        WHERE NAME IN ('Python', 'C#')
+    ) > 0
+ORDER BY ID;
