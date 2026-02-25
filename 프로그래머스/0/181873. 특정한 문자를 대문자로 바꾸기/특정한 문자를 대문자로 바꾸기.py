f30d2f6c6f380ebf5f60145760f060385de18550
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-02-25 20:56:30 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
@@ -1,10 +1,2 @@
 def solution(my_string, alp):
-    answer = ''
-    
-    for c in my_string:
-        if c == alp:
-            answer += c.upper()
-        else:
-            answer += c
-    
-    return answer
+    return my_string.replace(alp, alp.upper())
