ed643bd8b897b4c1e8ea025b2a44e28b4c548721
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-02-27 22:13:09 +0900

    [level 0] Title: l로 만들기, Time: 18.32 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,10 +1,2 @@
 def solution(myString):
-    answer = ''
-    
-    for c in myString:
-        if ord(c) < ord('l'):
-            answer += 'l'
-        else:
-            answer += c
-    
-    return answer
+    return ''.join('l' if ord(c) < ord('l') else c for c in myString)
