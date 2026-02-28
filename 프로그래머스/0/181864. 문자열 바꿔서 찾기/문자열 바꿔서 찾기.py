acba2ad4c2a82f40539b7b80d0beccb871105e98
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-02-28 21:47:06 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -3,4 +3,4 @@ def solution(myString, pat):
     myString = myString.replace('B', 'A')   
     myString = myString.replace('T', 'B')
     
-    return 1 if pat in myString else 0
+    return int(pat in myString)
