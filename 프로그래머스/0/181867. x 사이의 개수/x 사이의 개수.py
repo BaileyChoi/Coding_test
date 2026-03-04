5eaf63e4374357fd31a202a710336dfe586dad3b
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-03-05 00:08:29 +0900

    [level 0] Title: x 사이의 개수, Time: 2.21 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,10 +1,2 @@
 def solution(myString):
-    answer = []
-    
-    x = 0
-    for i, c in enumerate(myString):
-        if c == 'x':
-            answer.append(i - x)
-            x = i + 1
-    
-    return answer + [len(myString) - x]
+    return [len(s) for s in myString.split('x')]
