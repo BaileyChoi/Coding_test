40a950c07c7165186587cc760597aae670e6b42b
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-03-08 13:53:55 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,9 +1,2 @@
 def solution(binomial):    
-    a, b, c = binomial.split(' ')
-    
-    if b == '+':
-        return int(a) + int(c)
-    elif b == '-':
-        return int(a) - int(c)
-    else:
-        return int(a) * int(c)
+    return eval(binomial)
