0ab64e743a6854dfef90495d73aebdeb86a9743e
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-03-08 14:03:47 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.45 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,5 +1,5 @@
 def solution(arr, idx):
-    for i in range(idx, len(arr)):
-        if arr[i] == 1:
-            return i
-    return -1
+    try:
+        return arr[idx:].index(1) + idx
+    except:
+        return -1
