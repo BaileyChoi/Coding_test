d964959192fae3200bb35a827b373dd116848286
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-03-20 21:40:31 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -2,12 +2,8 @@ def solution(num_list):
     answer = 0
     
     for n in num_list:
-        count = 0   
-        
         while n != 1:
             n //= 2
-            count += 1
+            answer += 1
         
-        answer += count
-    
     return answer
