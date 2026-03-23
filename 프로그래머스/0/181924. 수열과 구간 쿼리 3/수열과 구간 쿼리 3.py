5c1cbbf4c8fcdeb9273f001481d517f9ed34dab9
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-03-23 22:30:30 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.11 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,10 +1,5 @@
 def solution(arr, queries):
-    def switch(i, j):
-        temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-    
     for i, j in queries:
-        switch(i, j)
+        arr[i], arr[j] = arr[j], arr[i]
     
     return arr
