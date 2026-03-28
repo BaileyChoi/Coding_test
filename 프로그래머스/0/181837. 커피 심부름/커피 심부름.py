5c12564cf0b1c671fe301b8c1e1aa2e62608869b
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-03-28 22:51:49 +0900

    [level 0] Title: 커피 심부름, Time: 0.13 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -2,9 +2,7 @@ def solution(order):
     answer = 0
     
     for o in order:
-        if 'americano' in o:
-            answer += 4500
-        elif 'cafelatte' in o:
+        if 'cafelatte' in o:
             answer += 5000
         else:
             answer += 4500
