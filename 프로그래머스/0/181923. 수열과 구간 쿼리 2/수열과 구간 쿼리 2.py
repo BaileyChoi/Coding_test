05d96ee027b8db7cbfb2f37800f90b4f863d0291
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-04-05 21:54:03 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 10.76 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -2,15 +2,15 @@ def solution(arr, queries):
     answer = []
     
     for s, e, k in queries:
-        temp = []
+        min_val = float('inf')
         
         for i in range(s, e + 1):
-            if arr[i] > k:
-                temp.append(arr[i])
-        
-        if temp:
-            answer.append(min(temp))
-        else:
+            if arr[i] > k and arr[i] < min_val:
+                min_val = arr[i]
+                
+        if min_val == float('inf'):
             answer.append(-1)
+        else:
+            answer.append(min_val)
                 
     return answer
