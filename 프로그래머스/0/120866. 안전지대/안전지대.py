eff00b535235a6572a1bd857ba84eae909ac466c
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-05-06 22:16:43 +0900

    [level 0] Title: 안전지대, Time: 0.08 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,24 @@
+def solution(board):
+    answer = 0
+    n = len(board)
+    danger =  [[0] * n for _ in range(n)]
+    
+    dx = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
+    dy = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+    
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                for k in range(9):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    
+                    if 0 <= nx < n and 0 <= ny < n:
+                        danger[nx][ny] = 1
+                        
+    for i in range(n):
+        for j in range(n):                
+            if danger[i][j] == 0:
+                answer += 1
+                
+    return answer
