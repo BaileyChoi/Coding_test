7b0b7bd57a3fedbb5818f152ba65ba9972dd413e
BaileyChoi <71368635+BaileyChoi@users.noreply.github.com>
2026-05-07 20:31:31 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,9 @@
+import math
+
+def solution(numer1, denom1, numer2, denom2):
+    a = numer1 * denom2 + numer2 * denom1
+    b = denom1 * denom2
+    
+    g = math.gcd(a, b)
+    
+    return [a // g, b // g]
